8772aa232094d3958dfcee20a5ef98a8caad7982
ManthraSri <manthrasri2004@gmail.com>
2025-03-21 14:27:43 +0530

    added examples of arraymethods

---
+++ ArrayMethods/Example2.js
@@ -0,0 +1,17 @@
+let words = ["Banana", "Apple", "Cherry", "Mango"];
+let numbers2 = [30, 10, 50, 20];
+
+// Sort words
+words.sort();
+console.log(words); 
+
+// Sorting numbers 
+numbers2.sort();
+console.log(numbers2); 
+
+// Reversing the order
+words.reverse();
+console.log(words); 
+
+// Extracting part of an array
+console.log(numbers2.slice(1, 3)); 
