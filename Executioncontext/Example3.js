9d79cb26dafaf54523a6e9f770f4748ed5b69d1f
ManthraSri <manthrasri2004@gmail.com>
2025-03-21 16:44:48 +0530

    modified

--- Executioncontext/Example3.js
+++ Executioncontext/Example3.js
@@ -1,7 +1,7 @@
 function out() {
-    console.log("Outer Function");
+    console.log("Outer Function");  // OUTER FUNCTION EXECUTION CONTEXT
     function inside() {
-        console.log("Inner Function");
+        console.log("Inner Function"); // INNER FUNCTION EXECUTION CONTEXT
     }
  inside();}
 out(); 
