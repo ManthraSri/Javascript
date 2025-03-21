3485209b1cbf5513827f9785273feb557296732e
ManthraSri <manthrasri2004@gmail.com>
2025-03-21 16:42:58 +0530

    modifies

--- Basics/Example1.js
+++ Basics/Example1.js
@@ -6,3 +6,23 @@ let e;              // Undefined
 let f = {x: 1, y: 2}; // Object
 
 console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f);
+
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+    console.log("For Loop:", i);
+}
+
+// While loop
+let j = 1;
+while (j <= 3) {
+    console.log("While Loop:", j);
+    j++;
+}
+
+// Do-while loop
+let k = 1;
+do {
+    console.log("Do-While Loop:", k);
+    k++;
+} while (k <= 2);
--- Basics/Example3.js
+++ Basics/Example3.js
@@ -1,8 +1,8 @@
+
+//CONST PROPERTIES
 const person = { name: "Alice", age: 25 };
 person.age = 26;
 console.log(person);// can change properties insides cannot reassign whole
-
-
 
 const numbers = [1, 2, 3];
 numbers.push(4); 
